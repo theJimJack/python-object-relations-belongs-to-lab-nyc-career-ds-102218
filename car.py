d5f3b5dd1dab33348518d882fd8d032530ad0089
Jim Jack <jim_jack@Jims-MacBook-Pro.local>
2018-11-01 09:03:56 -0400

    new information 10-31-2018

--- car.py
+++ car.py
@@ -1,3 +1,4 @@
+
 class Car:
 
     _all = []
@@ -8,3 +9,57 @@ class Car:
         self._year = year
         self._owner = owner
         Car._all.append(self)
+
+    @classmethod
+    def all(cls):
+        return Car._all
+
+    @property
+    def make(self):
+        return self._make
+    @make.setter
+    def make(self, new_make):
+        self._make = new_make
+    @make.deleter
+    def make(self):
+        del self._make
+
+    @property
+    def model(self):
+        return self._model
+    @model.setter
+    def model(self, new_model):
+        self._model = new_model
+    @model.deleter
+    def model(self):
+        del self._model
+
+    @property
+    def year(self):
+        return self._year
+    @year.setter
+    def year(self, new_year):
+        self._year = new_year
+    @year.deleter
+    def year(self):
+        del self._year
+
+    @property
+    def owner(self):
+        return self._owner
+    @owner.setter
+    def owner(self, new_owner):
+        self._owner = new_owner
+    @owner.deleter
+    def owner(self):
+        del self._owner
+
+    @classmethod
+    def cars_driven_by(cls, title):
+        selected_objects = []
+        for car in cls._all:
+            if car.owner.name == title:
+                selected_objects.append(car)
+        return selected_objects
+        # from person import Person
+        # return list(filter(lambda x:x.occupation==title,Person._all))
--- person.py
+++ person.py
@@ -3,6 +3,46 @@ from car import Car
 
 class Person:
 
+    _all = []
+
     def __init__(self, name, occupation):
         self._name = name
         self._occupation = occupation
+        Person._all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, new_name):
+        self._name = new_name
+
+    @property
+    def occupation(self):
+        return self._occupation
+    @occupation.setter
+    def occupation(self, new_occupation):
+        self._occupation = new_occupation
+
+    @classmethod
+    def has_oldest_car(cls):
+        oldest_year = sorted(list(map(lambda x:x.year,Car._all)))[0]
+        for i in Car._all:
+            if i.year == oldest_year:
+                return i.owner.name
+
+    @classmethod
+    def drives_a(cls, car_make):
+        object_list = list(filter(lambda x:x.make==car_make,Car._all))
+        DA = []
+        for i in object_list:
+            DA.append(i.owner.name)
+        return DA
+
+    def drives_same_make_as_me(self):
+        jims_make = []
+        for i in Car._all:
+            if i.owner.name==self.name:
+                jims_make.append(i.make)
+        all_makes = Person.drives_a(jims_make[0])
+        return list(filter(lambda x: self.name != x,all_makes))
